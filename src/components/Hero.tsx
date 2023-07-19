10ef11e8bdf63ecf8a63c709fc6ad49a683b9086
Chloe Baker <chloe.baker@huler.io>
2023-07-19 23:30:06 +0100

    fix: style title, button and splodge

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { styled } from "styled-components";
 import NavBar from "./NavBar";
-import BlueSplodge from "./Images/BlueSplodge";
+import BlueSplodge from "./Images/BlueSplodge.svg";
 
 interface HeroProps {
   className?: string;
@@ -12,20 +12,33 @@ const Hero = ({ className }: HeroProps) => {
       <div>
         <h2>CHILLED GRAPE</h2>
         <NavBar />
-        <BlueSplodge />
+        <img src={BlueSplodge} alt="Splodge 1"></img>
       </div>
+
       <h1>LOREM IPSUM DOLOR SIT AMET.</h1>
+
       <button>Read More</button>
     </div>
   );
 };
 
 const StyledHero = styled(Hero)`
+  overflow: hidden;
+  margin: 0;
   div {
     h2 {
       padding-top: 6px;
+      padding-left: 69px;
       font-size: 36px;
       font-weight: 700;
+      color: white;
+    }
+    img {
+      position: absolute;
+      transform: rotate(-15deg);
+      top: -250px;
+      left: -80px;
+      z-index: -1;
     }
   }
 
@@ -33,10 +46,26 @@ const StyledHero = styled(Hero)`
     font-weight: 700;
     font-size: 64px;
   }
-  header {
-    h1 {
-      font-size: 36;
-    }
+
+  h1 {
+    padding: 180px 0 15px 364px;
+    width: 600px;
+    margin: 0;
+    font-size: 36;
+  }
+  button {
+    border-radius: 100px;
+    background: var(
+      --primary,
+      linear-gradient(90deg, #73e0a9 0%, #5b68df 100%)
+    );
+    border: none;
+    width: 249px;
+    height: 60px;
+    font-size: 22px;
+    font-weight: 600;
+    color: white;
+    margin-left: 364px;
   }
 `;
 export default StyledHero;
