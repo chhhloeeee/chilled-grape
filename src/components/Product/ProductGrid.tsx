4f338f539ccb3d4a92dc8d509afca3b9002a671c
Chloe Baker <chloe.baker@huler.io>
2023-07-25 11:56:42 +0100

    fix: remove grid container wrapper

--- src/components/Product/ProductGrid.tsx
+++ src/components/Product/ProductGrid.tsx
@@ -1,39 +1,36 @@
 import ProductCard from "./ProductCard/ProductCard";
 import Car from "../Images/car.png";
 import { ProductGridWrapper } from "./ProductGridWrapper";
-import { ProductGridContainerWrapper } from "./ProductGridContainerWrapper";
 
 const ProductGrid = () => {
   return (
-    <ProductGridContainerWrapper>
-      <ProductGridWrapper>
-        <ProductCard
-          image={Car}
-          imageAlt="Car"
-          title="consectetur Elit"
-          price="£12.99"
-        />
-        <ProductCard
-          image={Car}
-          imageAlt="Car"
-          title="consectetur Elit"
-          price="£12.99"
-        />
-        <ProductCard
-          image={Car}
-          imageAlt="Car"
-          title="consectetur Elit"
-          price="£12.99"
-        />
-        <ProductCard
-          image={Car}
-          imageAlt="Car"
-          title="consectetur Elit"
-          price="£12.99"
-        />
-        <button>Load More</button>
-      </ProductGridWrapper>
-    </ProductGridContainerWrapper>
+    <ProductGridWrapper>
+      <ProductCard
+        image={Car}
+        imageAlt="Car"
+        title="consectetur Elit"
+        price="£12.99"
+      />
+      <ProductCard
+        image={Car}
+        imageAlt="Car"
+        title="consectetur Elit"
+        price="£12.99"
+      />
+      <ProductCard
+        image={Car}
+        imageAlt="Car"
+        title="consectetur Elit"
+        price="£12.99"
+      />
+      <ProductCard
+        image={Car}
+        imageAlt="Car"
+        title="consectetur Elit"
+        price="£12.99"
+      />
+      <button>Load More</button>
+    </ProductGridWrapper>
   );
 };
 
